fee2fc459a6b5de06fe0ffe6c15f1b98894bd8dd
Corelio <100584937+Coreli0@users.noreply.github.com>
2024-05-28 22:53:24 +0200

    Update script.js
    Signed-off-by: Corelio <100584937+Coreli0@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
-function performSearch() {
+function performSearch(event) {
+    event.preventDefault();
+
     var searchInput = document.getElementById('searchInput').value;
     var searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(searchInput);
-    window.location.href = searchUrl;
-    return false;
-}
-function openNotification() {
-            document.getElementById('overlay').classList.add('active');
-            document.getElementById('notification').classList.add('active');
-        }
 
-        function closeNotification() {
-            document.getElementById('overlay').classList.remove('active');
-            document.getElementById('notification').classList.remove('active');
-        }
+    var animationDiv = document.getElementById('animation');
+    animationDiv.style.display = 'flex';
+    setTimeout(function() {
+        window.location.href = searchUrl;
+    }, 4000);
+}
+
+function openNotification() {
+    document.getElementById('overlay').classList.add('active');
+    document.getElementById('notification').classList.add('active');
+}
+
+function closeNotification() {
+    document.getElementById('overlay').classList.remove('active');
+    document.getElementById('notification').classList.remove('active');
+}
+
+
+
+
+
+function runAnimation() {
+  var dot1 = document.getElementById('dot1');
+  var dot2 = document.getElementById('dot2');
+  var dot3 = document.getElementById('dot3');
+  var scale = 4;
+  var originalScale = 1; 
+  var duration = 500;
+  
+  function animateDot1() {
+    dot1.style.transform = 'scaleY(' + scale + ')';
+    setTimeout(function() {
+      dot1.style.transform = 'scaleY(' + originalScale + ')';
+      animateDot2();
+    }, duration);
+  }
+
+  function animateDot2() {
+    dot2.style.transform = 'scaleY(' + scale + ')';
+    setTimeout(function() {
+      dot2.style.transform = 'scaleY(' + originalScale + ')';
+      animateDot3();
+    }, duration);
+  }
+
+  function animateDot3() {
+    dot3.style.transform = 'scaleY(' + scale + ')';
+    setTimeout(function() {
+      dot3.style.transform = 'scaleY(' + originalScale + ')';
+      setTimeout(runAnimation, 500);
+    }, duration);
+  }
+  animateDot1();
+}
+
+window.onload = runAnimation;
